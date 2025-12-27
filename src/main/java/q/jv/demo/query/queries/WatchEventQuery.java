cbda31e5d9e024d9020455d6faada6ce9175f29d
Youssef EL HAMRI <youssef.elhamri.2002@gmail.com>
2025-12-27 13:55:13 +0100

    Added a WatchEventQuery

---
+++ src/main/java/q/jv/demo/query/queries/WatchEventQuery.java
@@ -0,0 +1,10 @@
+package q.jv.demo.query.queries;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class WatchEventQuery {
+    private String accountId;
+}
