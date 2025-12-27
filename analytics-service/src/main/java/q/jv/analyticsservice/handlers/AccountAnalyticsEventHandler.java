396bf477fcc7c584f0c06f13e624418fd9cb7aa0
Youssef EL HAMRI <youssef.elhamri.2002@gmail.com>
2025-12-27 23:49:12 +0100

    Added an AccountAnalyticsEventHandler

---
+++ analytics-service/src/main/java/q/jv/analyticsservice/handlers/AccountAnalyticsEventHandler.java
@@ -0,0 +1,10 @@
+package q.jv.analyticsservice.handlers;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AccountAnalyticsEventHandler {
+}
