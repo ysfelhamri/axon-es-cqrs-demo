c2f533e607a3e3f13436f2269b87531cb0418bb7
Youssef EL HAMRI <youssef.elhamri.2002@gmail.com>
2025-12-27 23:40:17 +0100

    Added an analytics micro-service

---
+++ analytics-service/src/main/java/q/jv/analyticsservice/AnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package q.jv.analyticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticsServiceApplication.class, args);
+    }
+
+}
