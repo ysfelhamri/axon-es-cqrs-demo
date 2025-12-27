5fc7beea09821f8f584a9e924bb8d1a41ffac618
Youssef EL HAMRI <youssef.elhamri.2002@gmail.com>
2025-12-27 11:45:10 +0100

    Added a command handler for the CreditAccountCommand and an event handler for the AccountCreditedEvent

--- src/main/java/q/jv/demo/commands/aggregates/AccountAggregate.java
+++ src/main/java/q/jv/demo/commands/aggregates/AccountAggregate.java
@@ -7,8 +7,10 @@ import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 import q.jv.demo.commands.commands.AddAccountCommand;
+import q.jv.demo.commands.commands.CreditAccountCommand;
 import q.jv.demo.enums.AccountStatus;
 import q.jv.demo.events.AccountCreatedEvent;
+import q.jv.demo.events.AccountCreditedEvent;
 
 @Aggregate
 @Slf4j
@@ -39,5 +41,26 @@ public class AccountAggregate {
         this.accountId = event.getAccountId();
         this.balance = event.getInitialBalance();
         this.status = event.getStatus();
+    }
+
+
+    @CommandHandler
+    public void handle(CreditAccountCommand command) {
+        log.info("############ CreditAccountCommand Received ############");
+        if(!status.equals(AccountStatus.ACTIVATED)) throw new RuntimeException("Account "+command.getId()+" must be activated");
+        if(command.getAmount()<=0) throw new IllegalArgumentException("Amount must be positive");
+        AggregateLifecycle.apply(new AccountCreditedEvent(
+                command.getId(),
+                command.getAmount(),
+                command.getCurrency()
+        ));
+    }
+
+    @EventSourcingHandler
+    public void on(AccountCreditedEvent event){
+        log.info("############ AccountCreditedEvent Occurred ############");
+
+        this.accountId = event.getAccountId();
+        this.balance += event.getAmount();
     }
 }
