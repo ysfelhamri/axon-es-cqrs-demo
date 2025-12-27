2ecec11c1eba1e4c9bf34973f5de05884747ee8b
Youssef EL HAMRI <youssef.elhamri.2002@gmail.com>
2025-12-27 11:34:07 +0100

    Added a CreditAccountCommand

---
+++ src/main/java/q/jv/demo/commands/commands/CreditAccountCommand.java
@@ -0,0 +1,13 @@
+package q.jv.demo.commands.commands;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Getter @AllArgsConstructor
+public class CreditAccountCommand {
+    @TargetAggregateIdentifier
+    private String id;
+    private double amount;
+    private String currency;
+}
