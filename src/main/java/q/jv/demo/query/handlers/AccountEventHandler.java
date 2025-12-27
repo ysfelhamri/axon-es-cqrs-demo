950b7bdb384994668d31baf9d15a470c9b2b1eba
Youssef EL HAMRI <youssef.elhamri.2002@gmail.com>
2025-12-27 12:59:26 +0100

    Added query event handlers for the AccountActivatedEvent and AccountStatusUpdateEvent

--- src/main/java/q/jv/demo/query/handlers/AccountEventHandler.java
+++ src/main/java/q/jv/demo/query/handlers/AccountEventHandler.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.eventhandling.EventMessage;
 import org.springframework.stereotype.Component;
+import q.jv.demo.events.AccountActivatedEvent;
 import q.jv.demo.events.AccountCreatedEvent;
+import q.jv.demo.events.AccountStatusUpdateEvent;
 import q.jv.demo.query.entities.Account;
 import q.jv.demo.query.repository.AccountRepository;
 import q.jv.demo.query.repository.OperationRepository;
@@ -30,6 +32,28 @@ public class AccountEventHandler {
                 .currency(event.getCurrency())
                 .createdAt(eventMessage.getTimestamp())
                 .build();
+        accountRepository.save(account);
+
+    }
+
+    @EventHandler
+    public void on(AccountActivatedEvent event){
+        log.info("Query side : AccountActivatedEvent Received");
+
+        Account account = accountRepository.findById(event.getAccountId()).get();
+        account.setStatus(event.getStatus());
+
+        accountRepository.save(account);
+
+    }
+
+    @EventHandler
+    public void on(AccountStatusUpdateEvent event){
+        log.info("Query side : AccountStatusUpdateEvent Received");
+
+        Account account = accountRepository.findById(event.getAccountId()).get();
+        account.setStatus(event.getStatus());
+
         accountRepository.save(account);
 
     }
