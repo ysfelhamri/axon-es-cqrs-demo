98be1a428531e44f3821ab5bcbf6b7100947939d
Youssef EL HAMRI <youssef.elhamri.2002@gmail.com>
2025-12-27 11:35:41 +0100

    Added a creditAccount method to handle sending a CreditAccountCommand

--- src/main/java/q/jv/demo/commands/controllers/AccountCommandController.java
+++ src/main/java/q/jv/demo/commands/controllers/AccountCommandController.java
@@ -3,7 +3,9 @@ package q.jv.demo.commands.controllers;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.web.bind.annotation.*;
 import q.jv.demo.commands.commands.AddAccountCommand;
+import q.jv.demo.commands.commands.CreditAccountCommand;
 import q.jv.demo.dtos.AddNewAccountRequestDTO;
+import q.jv.demo.dtos.CreditAccountRequestDTO;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -15,11 +17,22 @@ public class AccountCommandController {
     public AccountCommandController(CommandGateway commandGateway) {
         this.commandGateway = commandGateway;
     }
+
     @PostMapping("/add")
     public CompletableFuture<String> addNewAccount(@RequestBody AddNewAccountRequestDTO request){
         CompletableFuture<String> response =  commandGateway.send(new AddAccountCommand(
                 UUID.randomUUID().toString(),
                 request.initialBalance(),
+                request.currency()
+        ));
+        return response;
+    }
+
+    @PostMapping("/credit")
+    public CompletableFuture<String> creditAccount(@RequestBody CreditAccountRequestDTO request){
+        CompletableFuture<String> response =  commandGateway.send(new CreditAccountCommand(
+                request.accountId(),
+                request.amount(),
                 request.currency()
         ));
         return response;
